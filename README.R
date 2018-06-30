5313157a253960bb2d0a6708021ea5a8096d5efe
Bari Bookout <bari@hishma.com>
2018-06-30 15:03:48 -0700

    adding README.R file to local repo

---
+++ README.R
@@ -0,0 +1,7 @@
+analyzes gun murders as reported by FBI
+
+download-data.R   downloads data as a CSV file in data directory
+
+wrangle-data.R  creates a derived dataset and saves as R object in rda directory.
+
+analysis.R  reorders data by states with highest murders and saves bar graph in figs directory.
